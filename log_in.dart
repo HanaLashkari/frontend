bba47df75f4692b6d66359f6d7c2420d2184ae2c
HanaLashkari <h.l.farzan7@gmail.com>
2024-06-10 13:29:54 +0330

    login page compeletd

--- log_in.dart
+++ log_in.dart
@@ -16,19 +16,20 @@ class _page_oneState extends State<page_one> {
   final passwordController = TextEditingController();
   bool _isVisible = true;
   String response = '';
+  bool answerMassege = false;
   static const buttonColor = Color(0xffbb0000);
   static const Color colorText = Color(0xFF024335);
 
   @override
   Widget build(BuildContext context) {
-
+    double widthOfScreen = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Log In Page'),
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back,
-            color: buttonColor,
+            color: colorText,
           ), onPressed: () {  //todo
              },
         ),
@@ -99,11 +100,48 @@ class _page_oneState extends State<page_one> {
             Positioned(
                 top: 380,
                 right: 32,
-                child: FieldBox(
-                  labelText: 'رمز عبور',
-                  hintText: 'رمز عبور نباید شامل اسم کاربر باشد',
-                  controller: passwordController,
-                )
+                child: SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    obscureText: _isVisible,
+                    controller: passwordController,
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isVisible = !_isVisible;
+                          });
+                        },
+                        icon: _isVisible
+                            ? Icon(Icons.visibility_off)
+                            : Icon(Icons.visibility),
+                        color: colorText,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white70,
+                      labelText: 'رمز عبور',
+                      labelStyle: const TextStyle(
+                        color: Color(0xff003b11),
+                        fontWeight: FontWeight.w300,
+                      ),
+                      border: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                      hintText: 'رمز عبور نباید شامل نام کاربری باشد',
+                      hintStyle: const TextStyle(color: Color(0xFFB4B4B4)),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Color(0xff006716)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                            color: Color(0xff09ce36),
+                            style: BorderStyle.solid),
+                      ),
+                    ),
+                  ),
+                ),
             ),
             Positioned(
                 top: 450,
@@ -130,20 +168,12 @@ class _page_oneState extends State<page_one> {
               child: InkWell(
                 onTap: () async {
                   logIn();
-                  if (response == 'رمز مورد تایید است!') {
+                  print('************here     :    $response');
+                  if (response == '11') {
                     Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
                         builder: (context) => const InfoScreen(),
-                      ),
-                    );
-                  }
-                  else{
-                    Text(
-                      response,
-                      style: TextStyle(
-                          color: Color(0xbd961d6b),
-                          fontSize: 15
                       ),
                     );
                   }
@@ -167,6 +197,24 @@ class _page_oneState extends State<page_one> {
                   ),
                 )
               )),
+            response == '10'? Positioned(
+              top: 560,
+                right: widthOfScreen * 0.138,
+                child: PharseText(
+                  pharse: 'رمز عبور اشتباه است دوباره امتحان کنید',
+                  size: 17,
+                  color: buttonColor,
+                )
+            ) : response == '01' || response == '00' ?
+            Positioned(
+                top: 560,
+                right: widthOfScreen * 0.21,
+                child: PharseText(
+                  pharse: 'اطلاعات یافت نشد، ثبت نام کنید',
+                  size: 17,
+                  color: buttonColor,
+                )
+            ) : Positioned(child: Container()),
           ],
         ),
       ),
@@ -174,7 +222,7 @@ class _page_oneState extends State<page_one> {
   }
 
   Future<String> logIn() async {
-    await Socket.connect("192.168.128.145", 8000).then((serverSocket) {
+    await Socket.connect("192.168.1.35", 8000).then((serverSocket) {
       serverSocket
           .write('${usernameController.text}-${idController.text}-${passwordController.text}\u0000');
       serverSocket.flush();
